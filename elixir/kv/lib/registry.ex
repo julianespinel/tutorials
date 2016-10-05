80d5f86dd7f5fdfb4c4568095f922ad702c4122a
Julian Espinel <julianespinel@gmail.com>
2016-10-04 21:09:02 -0500

    Use call instead of cast.

--- elixir/kv/lib/registry.ex
+++ elixir/kv/lib/registry.ex
@@ -15,7 +15,7 @@ defmodule KV.Registry do
   end
 
   def create(server, name) do
-    GenServer.cast(server, {:create, name})
+    GenServer.call(server, {:create, name})
   end
 
   def stop(server) do
@@ -30,16 +30,16 @@ defmodule KV.Registry do
     {:ok, {names, refs}}
   end
 
-  def handle_cast({:create, name}, {names, refs}) do
+  def handle_call({:create, name}, _form, {names, refs}) do
     case lookup(names, name) do
-      {:ok, _pid} ->
-        {:noreply, {names, refs}}
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
       :error ->
         {:ok, pid} = KV.Bucket.Supervisor.start_bucket
         ref = Process.monitor(pid)
         refs = Map.put(refs, ref, name)
         :ets.insert(names, {name, pid})
-        {:noreply, {names, refs}}
+        {:reply, pid, {names, refs}}
     end
   end
 
