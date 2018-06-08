5901df4a702dc1ee237fbc5eded2303f32dfb28e
Julian Espinel <julianespinel@gmail.com>
2018-06-08 07:29:37 -0500

    Complete tutorial up to: Routing.

--- elixir/phoenix/hello/lib/hello_web/channels/user_socket.ex
+++ elixir/phoenix/hello/lib/hello_web/channels/user_socket.ex
@@ -2,10 +2,10 @@ defmodule HelloWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", HelloWeb.RoomChannel
+  channel("room:*", HelloWeb.RoomChannel, via: [Phoenix.Transports.WebSocket])
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport(:websocket, Phoenix.Transports.WebSocket)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
--- elixir/phoenix/hello/lib/hello_web/router.ex
+++ elixir/phoenix/hello/lib/hello_web/router.ex
@@ -2,23 +2,48 @@ defmodule HelloWeb.Router do
   use HelloWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", HelloWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    get "/hello", HelloController, :index
-    get "/hello/:messenger", HelloController, :show
+    get("/", PageController, :index)
+    get("/hello", HelloController, :index)
+    get("/hello/:messenger", HelloController, :show)
+    # Nested resources: a one-to-many relationship (user -> posts)
+    resources("/users", UserController) do
+      resources("/posts", PostController)
+    end
+
+    resources("/reviews", ReviewController)
+  end
+
+  scope "/admin", HelloWeb.Admin, as: :admin do
+    pipe_through(:browser)
+
+    resources("/reviews", ReviewController)
+    resources("/images", ImageController)
+    resources("/users", UserController)
+  end
+
+  scope "/api", HelloWeb.Api, as: :api do
+    pipe_through(:browser)
+
+    scope "/v1", V1, as: :v1 do
+      resources("/reviews", ReviewController)
+      resources("/images", ImageController)
+      resources("/users", UserController)
+    end
   end
 
   # Other scopes may use custom stacks.
